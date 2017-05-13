adb19be1c8f3e8659e2cf729450b3a9900868abc
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-05-13 21:13:34 +0200

    add config loading, logger and rubypress stuff to compostr.rb

--- lib/compostr.rb
+++ lib/compostr.rb
@@ -1,13 +1,30 @@
 require "compostr/version"
 
+require 'ostruct'
+require 'yaml'
+
 require 'compostr/custom_field_value'
 require 'compostr/custom_post_type'
 
 require 'compostr/entity_cache'
 
+# Get the loggers, get the config, expose WP
 module Compostr
+  def self.load_conf
+    @@config = OpenStruct.new YAML.load_file 'compostr.conf'
+  end
+  def self.config
+    @@config ||= load_conf
+  end
+
+  def self.wp
+    @wp ||= Rubypress::Client.new(host: config.host,
+                                  username: config.username,
+                                  password: config.password)
+  end
+
   def self.logger
-    @@logger || = Logger.new(STDOUT)
+    @@logger ||= Logger.new(STDOUT)
   end
   def self.logger= logger
     @@logger = logger
