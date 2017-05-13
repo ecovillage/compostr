43633fe5cfb8697f51b531a844c27370d816ccf5
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-05-13 15:45:06 +0200

    add EntityCache implementation

--- lib/compostr.rb
+++ lib/compostr.rb
@@ -3,6 +3,8 @@ require "compostr/version"
 require 'compostr/custom_field_value'
 require 'compostr/custom_post_type'
 
+require 'compostr/entity_cache'
+
 module Compostr
   def self.logger
     @@logger || = Logger.new(STDOUT)
