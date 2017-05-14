7a934cb3f9fb442397981cd70c42547af8a1cee8
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-05-14 10:45:17 +0200

    add some doc

--- lib/compostr.rb
+++ lib/compostr.rb
@@ -14,22 +14,30 @@ require 'compostr/syncer'
 
 # Get the loggers, get the config, expose WP
 module Compostr
+  # Load and memoize 'compostr.conf'.
   def self.load_conf
     @@config = OpenStruct.new YAML.load_file 'compostr.conf'
   end
+
+  # Access configuration hash.
   def self.config
     @@config ||= load_conf
   end
 
+  # Access (and/or initialize) Rubypress client, settings initially pulled
+  # from the configuration.
   def self.wp
     @wp ||= Rubypress::Client.new(host: config.host,
                                   username: config.username,
                                   password: config.password)
   end
 
+  # Access the logger, initialize and memoize it on demand.
   def self.logger
     @@logger ||= Logger.new(STDOUT)
   end
+
+  # Set the logger.
   def self.logger= logger
     @@logger = logger
   end
