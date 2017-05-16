f09939522cb0b761a8098dd64b308812cf0cc018
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-05-16 17:35:28 +0200

    implement WPString#wp_string

--- lib/compostr/wp_string.rb
+++ lib/compostr/wp_string.rb
@@ -1,7 +1,14 @@
 module Compostr
   module WPString
     def self.wp_string string
-      string
+      content = string.strip
+      if !string.to_s.match(/img/)
+        re = /<("[^"]*"|'[^']*'|[^'">])*>/
+        if content.gsub(re, '') == ''
+          return ''
+        end
+      end
+      content
     end
   end
 end
--- test/wp_string_test.rb
+++ test/wp_string_test.rb
@@ -4,7 +4,7 @@ class WPStringTest < Minitest::Test
   def test_strip
     assert_equal('both',  Compostr::WPString.wp_string(' both '))
     assert_equal('left',  Compostr::WPString.wp_string('  left'))
-    assert_equal('left',  Compostr::WPString.wp_string('right '))
+    assert_equal('right', Compostr::WPString.wp_string('right '))
     assert_equal('no ne', Compostr::WPString.wp_string('no ne '))
   end
 
@@ -13,6 +13,6 @@ class WPStringTest < Minitest::Test
     assert_equal('', Compostr::WPString.wp_string('<h1></h1>'))
     assert_equal('', Compostr::WPString.wp_string(' <strong><h1></h1></strong> '))
     assert_equal('<strong><h1>head</h1></strong>', Compostr::WPString.wp_string(' <strong><h1>head</h1></strong> '))
-    assert_equal('<img href=""/>', Compostr::WPString.wp_striCompostr::ng(' <img href=""/> '))
+    assert_equal('<img href=""/>', Compostr::WPString.wp_string(' <img href=""/> '))
   end
 end
