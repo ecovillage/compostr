56a1cba10911f6fe1ca62a3576856d2fe98ce2f7
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-05-18 11:49:04 +0200

    add vrc and webmock for integration kind of tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,12 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'compostr'
 
 require 'minitest/autorun'
+
+require 'webmock/minitest'
+
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'vcr_cassettes'
+  c.hook_into :webmock
+end
