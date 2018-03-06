0d157f366c8d2ab42fe1eef9ed7ece8149e945bb
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2018-03-06 11:04:15 +0100

    do not early fail finding featured_image if no image store is given

--- lib/compostr/image_uploader.rb
+++ lib/compostr/image_uploader.rb
@@ -16,14 +16,15 @@ module Compostr
     # Returns attachment id of an image already uploaded
     # or attachment_id of image after fresh upload
     # (or nil if path empty)
+    # path is relative path to file to upload.
     def process rel_path, wp_event=nil
       return nil if rel_path.to_s.strip.empty?
-      return nil if @image_store.nil?
 
       # Do we need URI encoding here?
       if attachment_id = @media_cache.id_of_name(rel_path)
         Compostr.logger.debug "Image already uploaded."
       else
+        return nil if @image_store.nil?
         path = File.join(@image_store, rel_path)
         Compostr.logger.info "Uploading file #{path}"
         upload = Compostr::ImageUpload.new(path)
